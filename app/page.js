416920776fbd6ffa9488e11632d9df0c90652a16
Supamid44m <supamid780@gmail.com>
2023-11-08 10:21:52 +0700

    update 1.0

--- app/page.js
+++ app/page.js
@@ -40,9 +40,9 @@ function Profile() {
 
   return (
     <>
-       <div className="absolute inset-x-0 top-0 w-full h-24  bg-yellow-200 ">
-        </div>
-      <div className='bg-white  rounded-md flex items-center max-w-md mx-auto mt-10 shadow  border-0 p-3 relative '>
+      <div className="absolute inset-x-0 top-0 w-full h-24  bg-yellow-200 ">
+      </div>
+      <div className='bg-white  rounded-2xl flex items-center max-w-md mx-auto mt-10 shadow  border-0 p-3 relative'>
         <form className="w-full relative pt-4 pb-4 pl-4 pr-4 ">
           <div className="relative">
             <input
@@ -59,80 +59,98 @@ function Profile() {
         </form>
       </div>
 
-      <div className='pl-12'>
-        <span className='border-s-2 pl-2 text-xl'>People</span>
-        <p className='pl-2'>
-          Search  results {filteredProfile.length} items of {profile.length}
-        </p>
-      </div>
-      <div className="grid grid-cols-4 gap-4 mb-10 place-items-center mt-2">
 
-        {filteredProfile.map((item, index) => (
-          <div
-            key={index}
-            className="block rounded-lg bg-white hover:bg-slate-300 relative shadow-md ml-2 mr-2 h-96 w-4/5 "
-          >
-            <div className="absolute inset-x-0 top-0 w-full h-20 bg-gradient-to-b from-indigo-500 rounded-t-lg">
-            </div>
+      <div className="">
 
-            <Image src={item.avatar} width="300" height="300" alt={item.username} className="rounded-full w-20 h-20 bg-slate-600 mt-6 mb-2 relative ml-2" />
+        <div className='pl-12'>
+          <span className='border-s-2 pl-2 text-xl'>People</span>
+          <p className=''>
+            Search  results {filteredProfile.length} items of {profile.length}
+          </p>
+        </div>
 
-            <div className='relative ml-2 '>
-              <p className="text-l pt-2">{item.first_name} {item.last_name}</p>
-              <p className="text-xs pb-2">{item.first_name}</p>
-            </div>
-
-            <div className='border-b-2 border-t-2 relative ml-2 mr-2  '>
-              <p className='text-xs pt-2'>Email : {item.email}</p>
-              <p className='text-xs pb-2'>Start date : {item.date_of_birth}</p>
-            </div>
-
-            <div className='relative ml-2'>
-              {Object.keys(item.subscription).length > 2 ? (
-                <>
-                  <div className='mb-2 '>
-                    <div className='justify-between'>
-                      <span className='text-l pt-2 flex flex-row'>
-                      <BiSolidShieldAlt2 />
-                      <p className='pl-2'>Guild</p>
-                    </span>
-                    </div>
-
-                    <span className='text-xs border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white'>{item.gender}</span>
-                    <span className='text-xs ml-2 border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white'>{item.address.country}</span>
-
-                    <Popup trigger={<button><span className='text-xs ml-2 border border-indigo-600 rounded-full hover:bg-indigo-800 hover:text-white'>...</span></button>}
-                      position="top center">
-                      <div className='bg-white flex flex-col my-2 w-52 border border-indigo-600 rounded-md'>
-                        <span className='text-xs border border-indigo-600 rounded-full h-5 px-2 my-2 mx-2 hover:bg-indigo-800 w-28 hover:text-white '>{item.gender}</span>
-                        <span className='text-xs border border-indigo-600 rounded-full h-5 px-2 my-2 mx-2 hover:bg-indigo-800 w-28 hover:text-white'>{item.address.country}</span>
-                      </div>
-                    </Popup>
-
-                  </div>
-
-
-                </>
-              ) : (
-                <>
-                  <span className='text-l pt-2 flex flex-row'> <BiSolidShieldAlt2 /> <p className='pl-2'>Guild</p> </span>
-                </>
-
-              )
-              }
-              <div className='mb-2'>
-                <span className='text-l flex flex-row'>
-                  <BsTagFill />
-                  <p className='pl-2'>Tag</p>
-                </span>
-                <span className='text-xs border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white '>{item.subscription.plan}</span>
+        <div className='grid grid-cols-4 gap-y-4 mb-10 place-items-center mt-2'>
+          {filteredProfile.map((item, index) => (
+            <div
+              key={index}
+              className="block rounded-2xl bg-white hover:bg-slate-300 relative shadow-md ml-2 mr-2 h-96 w-4/5  "
+            >
+              <div className="absolute inset-x-0 top-0 w-full h-20 bg-gradient-to-b from-indigo-500 rounded-t-2xl">
               </div>
 
+              <Image src={item.avatar} width="300" height="300" alt={item.username} className="rounded-full w-20 h-20 bg-slate-600 mt-6 mb-2 relative ml-2" />
 
+              <div className='relative ml-2 '>
+                <p className="text-l pt-2">{item.first_name} {item.last_name}</p>
+                <p className="text-xs pb-2">{item.first_name}</p>
+              </div>
+
+              <div className='border-b-2 border-t-2 relative ml-2 mr-2  '>
+                <p className='text-xs pt-2'>Email : {item.email}</p>
+                <p className='text-xs pb-2'>Start date : {item.date_of_birth}</p>
+              </div>
+
+              <div className='relative ml-2'>
+                {item.address.coordinates.lat > 2 ? (
+                  <>
+                    <div className='mb-2 mt-2'>
+
+
+                      <span className='flex items-center'>
+                        <BiSolidShieldAlt2 />
+                        <span className='pl-2'>Guild</span>
+                      </span>
+
+                      <div className='ml-4'>
+                        <span className='text-xs border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white'>{item.gender}</span>
+                        <span className='text-xs ml-2 border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white'>{item.address.country}</span>
+
+                        <Popup trigger={<button><span className='text-xs ml-2 border border-indigo-600 rounded-full hover:bg-indigo-800 hover:text-white'>...</span></button>}
+                          position="top center">
+                          <div className='bg-white flex flex-col my-2 w-40 border border-indigo-600 rounded-md'>
+                            <span className='text-xs border border-indigo-600 rounded-full h-5 px-2 py-a my-2 mx-2 hover:bg-indigo-800 w-28 hover:text-white'>{item.gender}</span>
+                            <span className='text-xs border border-indigo-600 rounded-full h-5 px-2 my-2 mx-2 hover:bg-indigo-800 w-28 hover:text-white'>{item.address.country}</span>
+                          </div>
+                        </Popup>
+
+                      </div>
+                    </div>
+
+
+                  </>
+                ) : (
+                  <>
+                    <span className='text-l pt-2 flex items-center '>
+                      <BiSolidShieldAlt2 />
+                      <span className='pl-2'>Guild</span>
+                    </span>
+                    <div className='ml-4'>
+                      <span className='text-xs border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white'>{item.gender}</span>
+                      <span className='text-xs ml-2 border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white'>{item.address.country}</span>
+
+                    </div>
+
+                  </>
+
+                )
+                }
+
+                <div className='mb-2 mt-2 '>
+                  <span className='text-l flex items-center'>
+                    <BsTagFill />
+                    <p className='pl-2'>Tag</p>
+                  </span>
+                  <div className='ml-4'>
+                    <span className='text-xs border border-indigo-600 rounded-full px-2 hover:bg-indigo-800 hover:text-white '>{item.subscription.plan}</span>
+                  </div>
+                </div>
+
+
+              </div>
             </div>
-          </div>
 
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
